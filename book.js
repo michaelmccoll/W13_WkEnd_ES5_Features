7d8f2606b9e5057aae1b174859e4c837bd16d2fd
michaelmccoll <mccollmichael@hotmail.com>
2021-05-23 17:41:13 +0100

    Completed homework

---
+++ book.js
@@ -0,0 +1,14 @@
+export default class Book {
+  constructor(author, title, genre){
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+  }
+}
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+
+// module.exports = Book;
